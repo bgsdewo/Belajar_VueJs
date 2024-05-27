d453bd10c49e26e4ec83e077a6ab33f83f352c8e
unknown <bgsdewoo20@gmail.com>
2024-05-27 21:44:50 +0700

    Belajar binding data ke dalam atribute html

--- js/vue.js
+++ js/vue.js
@@ -1,13 +1,18 @@
+//Materi Reactive Data pada VueJs
+
 var products = {
-    "Title": "Naruto Shipuden",
-    "Rating": "80/100",
-
-};
-
-
+    "name": "T-Shirt Putih",
+    "price": 150000,
+    "description": "T-shirt putih berbahan katun yang nyaman dipakai sehari-hari.",
+    "image": "https://via.placeholder.com/150?text=T-Shirt+Putih"
+}
 
 var app = new Vue({
     el:'#apps',
     data:products
 })
 
+//Materi Cara Binding Data ke dalam Attribute HTML
+// v-bind -> menggunakan itu cara binding data ke HTML
+// Tanpa pake {{ }} atau ada cara cepatnya
+// :src="image" v-bind:alt="name">

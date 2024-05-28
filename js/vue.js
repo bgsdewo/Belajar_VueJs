888f46bab785aefe890f1ce1a8996f2543e37800
unknown <bgsdewoo20@gmail.com>
2024-05-28 23:35:28 +0700

    CHAPTER 2  & belajar Interpolation Template

--- js/vue.js
+++ js/vue.js
@@ -1,61 +1,12 @@
-//Materi Reactive Data pada VueJs
 var app = new Vue({
-    el: '#app',
+    el:'#app',
     data: {
-        maximum: 50,
-        products: null,
-        cart: []
-    },
-    mounted: function() {
-        fetch('https://hplussport.com/api/products/order/price')
-        .then(response => response.json())
-        .then(data => {
-            this.products = data;
-        });
-    },
-    methods: {
-        addItem: function (product) {
-            this.cart.push(product);
-        }
+        name:'Flufy ski cat'
     }
-});
-
-//Materi Cara Binding Data ke dalam Attribute HTML
-// v-bind -> menggunakan itu cara binding data ke HTML
-// Tanpa pake {{ }} atau ada cara cepatnya
-// :src="image" v-bind:alt="name">
-
-
-//Materi Membuat Perulangan di dalam VueJS
-//v-for="item in products" menggunakan itu ketika kita mempunyai 
-// objek yang bersarang seperti di var dataProduct
-// yang artinya variabel temporarynya itu item dan kita ingin mendapatkan
-// data itu didalam property apa (in products),disini kita make didalam products
-
-//Materi Fungsi Kondisi di dalam VueJS
-// jadi di VueJs juga bisa menggunakan pengkondisian if else
-// misalnya dengan menggunakan v-if="item.price >= Number(maximum)"
-
-//Materi Menangani Input Data dari User
-// v-model="maximum",menggunakan itu kita bisa menghandel inputan user
-
-
-//Materi Mengenal Lifecyle Hooks VueJS
-// var app = new Vue({
-//     el:'#apps',
-//     data: {
-//         maximum:50,
-//         products:null,
-//     },
-//     mounted:function() {
-//         fetch('https://hplussport.com/api/products/order/price')
-//         .then(Response => Response.json())
-//         .then(data => {
-//             this.products = data;
-//         });
-//     }
-// }) kita bisa melakukan ini tanpa menulis hardcode pada objek sebelumnya
-
-
-//Materi Mengenal Event dan Method di dalam VueJS
-// v-on:click="addItem(item)"
+})
+//Materi Interpolation Template
+// jadi di materi ini kita dapat memanipulasi elemen html
+// dengan script" css dengan menggunakan v-html
+// contoh: <p><bold>Saya</bold> makan</p>,dan
+// menggunakan v-once kita tidak akan memanipulasi
+// elemen html tersebut meskipun kita sudah merubah isinya

b3755b0d37db4a297299194e1a44deddbf6047f1
unknown <bgsdewoo20@gmail.com>
2024-05-29 21:35:13 +0700

    Styling Secara Terprogram dengan Vue dan membenarkan program yang benar

--- js/vue.js
+++ js/vue.js
@@ -1,28 +1,29 @@
-new Vue({
+var app = new Vue({
     el: '#app',
     data: {
         maximum: 50,
-        products: [],
+        products: null,
         cart: [],
         style: {
-            label: ['font-weight-bold', 'mr-2'],
-            inputWidth: 60
+            label: 'font-weight-bold mr-2',
+            inputWidth: '60px',
+            sliderStatus: true
         }
+    },
+    mounted: function() {
+        fetch('https://hplussport.com/api/products/order/price')
+        .then(response => response.json())
+        .then(data => {
+            this.products = data;
+        });
     },
     computed: {
-        filteredProducts() {
-            return this.products.filter(item => item.price <= this.maximum);
+        sliderState:function(){
+            return this.style.sliderStatus ? 'd-flex' : 'd-none'
         }
     },
-    mounted() {
-        fetch('https://hplussport.com/api/products/order/price')
-            .then(response => response.json())
-            .then(data => {
-                this.products = data;
-            });
-    },
     methods: {
-        addItem(product) {
+        addItem: function (product) {
             this.cart.push(product);
         }
     }
